1a214d15c06ec9022fba0488725229cbca10d27d
lebe24 <emmanuel.philipel@yahoo.com>
2024-05-27 16:50:52 +0100

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Lebemac on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
